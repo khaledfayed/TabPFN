3a6a4da82ffb2e6cdc331612c646610259b40b5b
Khaled Fayed <khaledfayed1995@gmail.com>
2023-07-02 16:08:41 +0200

    update evaluate function

--- tabpfn/evaluate_classifier.py
+++ tabpfn/evaluate_classifier.py
@@ -4,24 +4,25 @@ from sklearn.metrics import accuracy_score
 import time
 import torch
 
-def evaluate_classifier(classifier, dids):
+def evaluate_classifier(classifier, dids, train_data=0.6):
     
     datasets = load_OHE_dataset(dids)
     
     for dataset in datasets:
-        
-        fit_batch = meta_dataset_loader([dataset], num_samples_per_class=5, one_batch=True, shuffle=False)
+        # fit_batch = meta_dataset_loader([dataset], num_samples_per_class=5, one_batch=True, shuffle=False)
+        fit_data = dataset['data'][:int(len(dataset['data'])*train_data)]
+        fit_target = dataset['target'][:int(len(dataset['data'])*train_data)]
         start = time.time()
-        classifier.fit(fit_batch[0]['x'], fit_batch[0]['y'])
+        classifier.fit(fit_data, fit_target)
         y_eval, p_eval = classifier.predict(dataset['data'], return_winning_probability=True)
         accuracy = accuracy_score(dataset['target'], y_eval)
-        print('Dataset ID:',dataset['id'], 'Prediction time: ', time.time() - start, 'Accuracy', accuracy, '\n')
+        print('Dataset ID:',dataset['id'], 'Shape:', dataset['data'].shape, 'Prediction time: ', time.time() - start, 'Accuracy', accuracy, '\n')
         return accuracy
         
 def main():
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     classifier = TabPFNClassifier(device=device, N_ensemble_configurations=4, only_inference=False)
-    evaluate_classifier(classifier, [22])
+    evaluate_classifier(classifier, [40966])
     pass
 
 if __name__ == "__main__":

d0b53eb1d86b77c84e3f7ad45a6c161be921594a
Khaled Fayed <khaledfayed1995@gmail.com>
2023-07-18 13:03:37 +0200

    add accuracy

--- tabpfn/debug_train.py
+++ tabpfn/debug_train.py
@@ -5,6 +5,7 @@ from scripts.transformer_prediction_interface import TabPFNClassifier
 from meta_dataset_loader import load_OHE_dataset, meta_dataset_loader2, split_datasets
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score
 
 
 def train():
@@ -25,16 +26,18 @@ def train():
     
     for e in range(epochs):
         # y_query = label_encoder.fit_transform(y_query)
-            
         classifier.fit(x_support, y_support)
+        y_eval, p_eval = classifier.predict(x_query, return_winning_probability=True)
+        accuracy = accuracy_score(y_query, y_eval)
+            
         optimizer.zero_grad()
         prediction = classifier.predict_proba2(x_query)
         prediction = prediction.squeeze(0)
         loss = criterion(prediction,torch.from_numpy(y_query).to(device))
-        print('epoch',e,'|','loss =',loss.item())
+        print('epoch',e,'|','loss =',loss.item(),'|','acc =',accuracy)
         loss.backward()
         optimizer.step()
-        accumulator += loss.item()
+        
     
 def main():
     train()

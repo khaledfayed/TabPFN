fbb8d817e1128996ced8a1a5e8ab0b69c9e1eaea
Khaled Fayed <khaledfayed1995@gmail.com>
2023-08-12 23:24:14 +0200

    debug device error

--- tabpfn/train_new_try_meta.py
+++ tabpfn/train_new_try_meta.py
@@ -117,7 +117,9 @@ def train(lr=0.00001, wandb_name='', num_augmented_datasets=0):
                 
                 output = model((None, X_full, y_full) ,single_eval_pos=eval_pos)[:, :, 0:num_classes] #TODO: check if we need to add some sort of style
                 # output = torch.nn.functional.softmax(output, dim=-1)
-                
+                x = torch.from_numpy(query_dataset[i]['y']).to(device).long().flatten()
+                y = output.reshape(-1, num_classes)
+                print(x.device, y.device, device)
                 losses = criterion(output.reshape(-1, num_classes), torch.from_numpy(query_dataset[i]['y']).to(device).long().flatten())
                 losses = losses.view(*output.shape[0:2])
                 

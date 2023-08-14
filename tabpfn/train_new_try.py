f5033d9f2cf1e864d8c4e90d3b271591d12560b0
Khaled Fayed <khaledfayed1995@gmail.com>
2023-08-14 17:36:24 +0200

    rewrite

--- tabpfn/train_new_try.py
+++ tabpfn/train_new_try.py
@@ -136,7 +136,10 @@ def train(lr=0.00001, wandb_name='', num_augmented_datasets=0, epochs = 100):
         optimizer.step()
         # accuracy = evaluate_classifier2(classifier, datasets)    
         optimizer.zero_grad()  
-    top=[ (i, predictions[i]) if predictions[i]>100 else None for i in range(len(predictions))]            
+    top= []
+    for i in range(len(predictions)):
+        if predictions[i]>100:
+            top.append((i, predictions[i]))
     print(predictions)  
     print(top)
         

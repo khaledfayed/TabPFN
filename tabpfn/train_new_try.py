681d7990280dd1c0158cae5718c9205ec58a6cd2
Khaled Fayed <khaledfayed1995@gmail.com>
2023-08-15 13:14:35 +0200

    fix bug

--- tabpfn/train_new_try.py
+++ tabpfn/train_new_try.py
@@ -124,7 +124,6 @@ def train(lr=0.00001, wandb_name='', num_augmented_datasets=0, epochs = 100, wei
         
         with torch.no_grad():
             test_output = model((None, X_full_test, y_full) ,single_eval_pos=eval_pos)[:, :, 0:num_classes] #TODO: check if we need to add some sort of style
-            test_acc = accuracy_score( torch.from_numpy(y_test).long().flatten().cpu(), torch.argmax(test_output.reshape(-1, num_classes).detach().cpu(), axis=1) )
             prediction = torch.argmax(test_output.reshape(-1, num_classes).detach().cpu(), axis=1)
             predictions = [predictions[i] if prediction[i] == y_test[i] else predictions[i]+1 for i in range(len(prediction))]
             
@@ -132,6 +131,8 @@ def train(lr=0.00001, wandb_name='', num_augmented_datasets=0, epochs = 100, wei
             test_losses = test_losses.view(*test_losses.shape[0:2])
             test_loss, test_nan_share = utils.torch_nanmean(test_losses.mean(0), return_nanshare=True)
             
+            test_acc = accuracy_score( torch.from_numpy(y_test).long().flatten().cpu(), torch.argmax(test_output.reshape(-1, num_classes).detach().cpu(), axis=1) )
+
         loss, nan_share = utils.torch_nanmean(losses.mean(0), return_nanshare=True)
         acc = accuracy_score( torch.from_numpy(query_dataset[0]['y']).long().flatten().cpu(), torch.argmax(output.reshape(-1, num_classes).detach().cpu(), axis=1) )
         loss.backward()

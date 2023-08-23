612edb1875c9c7db7de44c7dee5ef9b4ef22fffb
Khaled Fayed <khaledfayed1995@gmail.com>
2023-08-23 15:07:47 +0200

    print tensor sizes

--- tabpfn/train_new_try_meta.py
+++ tabpfn/train_new_try_meta.py
@@ -125,10 +125,19 @@ def train(lr=0.0001, wandb_name='', num_augmented_datasets=0, epochs = 100, weig
                     
                     model.to(device)
                     
+                    print(X_full.shape)
+                    # print(X_full)
+                    
                     output = model((None, X_full, y_full) ,single_eval_pos=eval_pos)[:, :, 0:num_classes] #TODO: check if we need to add some sort of style
                     # output = torch.nn.functional.softmax(output, dim=-1)
                     label, out = torch.from_numpy(query_dataset[i]['y']).long().flatten().to(device), torch.argmax(output.reshape(-1, num_classes), axis=1)
                     if torch.all(torch.isin(label, out)) and torch.all(torch.isin(out, label)):
+                        
+                        print(torch.from_numpy(query_dataset[i]['y']).to(device).long().flatten().shape)
+                        # print(torch.from_numpy(query_dataset[i]['y']).to(device).long().flatten())
+                        print(output.reshape(-1, num_classes).shape)
+                        # print(output.reshape(-1, num_classes))
+                        
                         losses = criterion(output.reshape(-1, num_classes) , torch.from_numpy(query_dataset[i]['y']).to(device).long().flatten())
                         losses = losses.view(*output.shape[0:2])
                         

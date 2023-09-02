83281c1363547fa39a3829ee3e1d8b457a1ba60c
Khaled Fayed <khaledfayed1995@gmail.com>
2023-09-02 16:00:59 +0200

    print has nans or not

--- tabpfn/train_new_try_meta.py
+++ tabpfn/train_new_try_meta.py
@@ -118,11 +118,14 @@ def train(lr=0.0001, wandb_name='', num_augmented_datasets=0, epochs = 100, weig
                 
                 model.to(device)
                 
+                print('X_full has nans', torch.isnan(X_full).any())
+                
                 
                 output = model((None, X_full, y_full) ,single_eval_pos=eval_pos)[:, :, 0:num_classes] #TODO: check if we need to add some sort of style
                 # output = torch.nn.functional.softmax(output, dim=-1)
                     
                 losses = criterion(output.reshape(-1, num_classes) , torch.from_numpy(query_dataset[i]['y']).to(device).long().flatten())
+                print('losses contaons nans', torch.isnan(losses).any())
                 losses = losses.view(*output.shape[0:2])
                 
                 loss, nan_share = utils.torch_nanmean(losses.mean(0), return_nanshare=True)

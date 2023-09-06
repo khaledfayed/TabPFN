549908becc6521b1caf33060f37c01c38dc366bc
Khaled Fayed <khaledfayed1995@gmail.com>
2023-09-06 17:48:38 +0200

    fix bug

--- tabpfn/train_new_try_meta.py
+++ tabpfn/train_new_try_meta.py
@@ -123,7 +123,7 @@ def train(lr=0.0001, wandb_name='', num_augmented_datasets=0, epochs = 100, weig
                 output = model((None, X_full, y_full) ,single_eval_pos=eval_pos)[:, :, 0:num_classes] #TODO: check if we need to add some sort of style
                 # output = torch.nn.functional.softmax(output, dim=-1)
                 
-                print('unique output', torch.argmax(torch.unique(output.reshape(-1, num_classes))))
+                print('unique output', torch.unique(torch.argmax(output.reshape(-1, num_classes))))
                 print('unique y', torch.unique(torch.from_numpy(query_dataset[i]['y']).to(device).long().flatten()))
                     
                 losses = criterion(output.reshape(-1, num_classes) , torch.from_numpy(query_dataset[i]['y']).to(device).long().flatten())

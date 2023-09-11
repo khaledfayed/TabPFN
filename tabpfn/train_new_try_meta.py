34680d537da9d5413edf75a715128c9f8060cdf9
Khaled Fayed <khaledfayed1995@gmail.com>
2023-09-11 17:35:15 +0200

    add schedular to main loop

--- tabpfn/train_new_try_meta.py
+++ tabpfn/train_new_try_meta.py
@@ -20,7 +20,7 @@ normalize_with_test= False
 normalize_with_sqrt= False
 normalize_to_ranking = False
 max_features = 100
-warmup_epochs=20
+warmup_epochs=100
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 def preprocess_input(eval_xs, eval_ys, eval_position):
         import warnings
@@ -81,7 +81,7 @@ def train(lr=0.0001, wandb_name='', num_augmented_datasets=0, epochs = 100, weig
     aggregate_k_gradients = config['aggregate_k_gradients']
     
     optimizer = optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
-    # scheduler = get_cosine_schedule_with_warmup(optimizer, warmup_epochs, epochs if epochs is not None else 100) # when training for fixed time lr schedule takes 100 steps
+    scheduler = get_cosine_schedule_with_warmup(optimizer, warmup_epochs, epochs if epochs is not None else 100) # when training for fixed time lr schedule takes 100 steps
     
     
     print('Start training')
@@ -147,6 +147,8 @@ def train(lr=0.0001, wandb_name='', num_augmented_datasets=0, epochs = 100, weig
                     torch.nn.utils.clip_grad_norm_(model.parameters(), 1.)
                     try:
                         optimizer.step()
+                        scheduler.step()
+                        if device != 'cpu': wandb.log({"lr":  optimizer.param_groups[0]['lr']})
                         with torch.no_grad():
                             
                             evaluate_classifier2(classifier, test_datasets, log= device != 'cpu')

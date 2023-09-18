96ff17aef3436c8adfe2fb4f17fce8048464365f
Khaled Fayed <khaledfayed1995@gmail.com>
2023-09-19 01:42:41 +0200

    change dir

---
+++ tabpfn/sai.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Define the partition on which the job shall run.
+#SBATCH --partition alldlc_gpu-rtx2080    # short: -p <partition_name>
+
+# Define a name for your job
+#SBATCH --job-name MosRL1             # short: -J <job name>
+
+# Define the files to write the outputs of the job to.
+# Please note the SLURM will not create this directory for you, and if it is missing, no logs will be saved.
+# You must create the directory yourself. In this case, that means you have to create the "logs" directory yourself.
+
+#SBATCH --output logs/%x-%A-HelloCluster.out   # STDOUT  %x and %A will be replaced by the job name and job id, respectively. short: -o logs/%x-%A-job_name.out
+#SBATCH --error logs/%x-%A-HelloCluster.err    # STDERR  short: -e logs/%x-%A-job_name.out
+
+# CPU settings
+#SBATCH --cpus-per-task=64
+#SBATCH --ntasks-per-core=1
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+
+# Define the amount of memory required per node
+#SBATCH --mem 128GB
+
+#SBATCH --gres=gpu:5
+
+
+echo "Workingdir: $PWD";
+echo "Started at $(date)";
+
+# A few SLURM variables
+echo "Running job $SLURM_JOB_NAME using $SLURM_JOB_CPUS_PER_NODE cpus per node with given JID $SLURM_JOB_ID on queue $SLURM_JOB_PARTITION";
+
+# Activate your environment
+# You can also comment out this line, and activate your environment in the login node before submitting the job
+source ~/.bashrc # Adjust to your path of Miniconda installation
+conda activate habitat
+
+
+# Running the job
+start=`date +%s`
+
+nvidia-smi
+python -m mos_habitat.train experiment=044_dirchlet_map_wo_unc
+
+end=`date +%s`
+runtime=$((end-start))
+
+echo Job execution complete.
+echo Runtime: $runtime

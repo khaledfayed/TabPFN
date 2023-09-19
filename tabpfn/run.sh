8180f035c0d149627d2c95cbb96f8c520b7e654d
Khaled Fayed <khaledfayed1995@gmail.com>
2023-09-19 17:40:00 +0200

    update run

--- tabpfn/run.sh
+++ tabpfn/run.sh
@@ -38,7 +38,7 @@ conda activate thesis
 # Running the job
 start=`date +%s`
 
-python train_new_try_meta.py --epochs 1000 --lr 0.0003 --weight_decay 0.0001 --name "first cluster run"
+python train_new_try_meta.py --epochs 1000 --lr 0.0001 --weight_decay 0.001 --name "second cluster run"
 
 end=`date +%s`
 runtime=$((end-start))

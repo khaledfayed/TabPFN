145d786b78042b55d155af25ad6febfb73c5c543
Khaled Fayed <khaledfayed1995@gmail.com>
2023-09-25 11:46:56 +0200

    increase lr

--- tabpfn/run.sh
+++ tabpfn/run.sh
@@ -38,7 +38,7 @@ conda activate thesis
 # Running the job
 start=`date +%s`
 
-python train_new_try_meta.py --epochs 1000 --lr 0.00003 --weight_decay 0.001 --name "fine tune"
+python train_new_try_meta.py --epochs 1000 --lr 0.0001 --weight_decay 0.001 --name "fine tune"
 
 end=`date +%s`
 runtime=$((end-start))

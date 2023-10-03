2ca9bb6424ca21371b406e80a9ac5b81ebbfe541
Khaled Fayed <khaledfayed1995@gmail.com>
2023-10-03 14:28:19 +0200

    update device

--- tabpfn/use_get_model.py
+++ tabpfn/use_get_model.py
@@ -1,6 +1,8 @@
 from scripts.transformer_prediction_interface import TabPFNClassifier
+import torch
 
 if __name__ == "__main__":
 
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    classifier = TabPFNClassifier(device='cpu', N_ensemble_configurations=1, only_inference=False)
+    classifier = TabPFNClassifier(device=device, N_ensemble_configurations=1, only_inference=False)

013c8218aca84f93a4fba1008024b4c8f6666346
Khaled Fayed <khaledfayed1995@gmail.com>
2023-10-03 14:25:36 +0200

    try get model

--- tabpfn/use_get_model.py
+++ tabpfn/use_get_model.py
@@ -1,4 +1,6 @@
 from scripts.transformer_prediction_interface import TabPFNClassifier
 
+if __name__ == "__main__":
 
-classifier = TabPFNClassifier(device='cpu', N_ensemble_configurations=1, only_inference=False)
+
+    classifier = TabPFNClassifier(device='cpu', N_ensemble_configurations=1, only_inference=False)

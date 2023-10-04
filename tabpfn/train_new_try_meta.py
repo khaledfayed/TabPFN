0e7576ab34e0927362203ac0e4bafd7cd60ded5b
Khaled Fayed <khaledfayed1995@gmail.com>
2023-10-04 16:06:11 +0200

    drop features

--- tabpfn/train_new_try_meta.py
+++ tabpfn/train_new_try_meta.py
@@ -206,7 +206,7 @@ if __name__ == "__main__":
     parser.add_argument("--name", type=str, help="The first argument (an integer)")
     args = parser.parse_args()
     
-    config = [('shuffle_features', 4)]
+    config = [('drop_features', 4)]
 
 
     

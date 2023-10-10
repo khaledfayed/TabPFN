62111c80d44b93eb6cadc8089d0361916f7ffc08
Khaled Fayed <khaledfayed1995@gmail.com>
2023-10-10 14:26:02 +0200

    update run

--- tabpfn/run.sh
+++ tabpfn/run.sh
@@ -38,7 +38,7 @@ conda activate thesis
 # Running the job
 start=`date +%s`
 
-python train_new_try_meta.py --epochs 1000 --lr 0.0001 --weight_decay 0.0001 --name "more batches"
+python train_new_try_meta.py --epochs 1000 --lr 0.0001 --weight_decay 0.0001 --name "more batches shuffle"
 
 end=`date +%s`
 runtime=$((end-start))
--- tabpfn/train_new_try_meta.py
+++ tabpfn/train_new_try_meta.py
@@ -202,9 +202,9 @@ if __name__ == "__main__":
     parser.add_argument("--name", type=str, help="The first argument (an integer)")
     args = parser.parse_args()
     
-    config = [('drop_features', 4),('shuffle_features', 2)]
+    config = [('shuffle_features', 2)]
 
 
     
-    train(wandb_name=args.name, epochs=args.epochs, lr=args.lr, weight_decay=args.weight_decay)    
+    train(wandb_name=args.name, epochs=args.epochs, lr=args.lr, weight_decay=args.weight_decay, augmentation_config=config)    
     # train()

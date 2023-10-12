4576208c3ac582fd507e37cf801c3d67f639978c
Khaled Fayed <khaledfayed1995@gmail.com>
2023-10-12 13:40:47 +0200

    1.b

--- tabpfn/run.sh
+++ tabpfn/run.sh
@@ -38,7 +38,7 @@ conda activate thesis
 # Running the job
 start=`date +%s`
 
-python train_new_try_meta.py --epochs 400 --lr 0.0001 --weight_decay 0.0001 --name "1.a_no_aug"
+python train_new_try_meta.py --epochs 400 --lr 0.0001 --weight_decay 0.0001 --name "1.b_shuffle"
 
 end=`date +%s`
 runtime=$((end-start))
--- tabpfn/train_new_try_meta.py
+++ tabpfn/train_new_try_meta.py
@@ -206,8 +206,8 @@ if __name__ == "__main__":
     parser.add_argument("--name", type=str, help="The first argument (an integer)")
     args = parser.parse_args()
     
-    # config = [('shuffle_features', 2)]
-    config = []
+    config = [('shuffle_features', 2)]
+    # config = []
 
 
     

212a439f1550120a3ceb3042e64c4291478d897c
Khaled Fayed <khaledfayed1995@gmail.com>
2023-10-13 11:45:02 +0200

    1.b.1

--- tabpfn/train_new_try_meta.py
+++ tabpfn/train_new_try_meta.py
@@ -207,8 +207,8 @@ if __name__ == "__main__":
     parser.add_argument("--name", type=str, help="The first argument (an integer)")
     args = parser.parse_args()
     
-    config = [('relabel', 2), ('shuffle_features', 1)]
-    # config = []
+    # config = [('relabel', 2), ('shuffle_features', 1)]
+    config = [('shuffle_features', 1)]
 
 
     

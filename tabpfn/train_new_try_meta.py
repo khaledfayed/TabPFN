9f75a16f71aad685f4509a6294610b62d75e8dfb
Khaled Fayed <khaledfayed1995@gmail.com>
2023-10-13 11:43:09 +0200

    save each 100

--- tabpfn/train_new_try_meta.py
+++ tabpfn/train_new_try_meta.py
@@ -183,7 +183,7 @@ def train(lr=0.0001, wandb_name='', num_augmented_datasets=0, epochs = 100, weig
         scheduler.step()
         if device != 'cpu': wandb.log({"lr":  optimizer.param_groups[0]['lr']})
         
-        if e % 50 == 0:
+        if e % 100 == 0:
             criterion.weight=torch.ones(10)
             model_save_name = f'{wandb_name}_e_{e}_lr_{lr}'
             checkpoint = f'prior_diff_real_checkpoint_{model_save_name}_n_0_epoch_100.cpkt'

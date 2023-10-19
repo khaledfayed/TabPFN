b5ec9214e73eb601bf0394623279746ae12bc62d
Khaled Fayed <khaledfayed1995@gmail.com>
2023-10-19 13:15:44 +0200

    try

--- tabpfn/lightning_train.py
+++ tabpfn/lightning_train.py
@@ -147,7 +147,7 @@ from evaluate_classifier import auto_ml_dids_train
 
 class TrainDataLoader(DataLoader):
     def __init__(self, datasets, *args, **kwargs):
-        super(TrainDataLoader, self).__init__(*args, **kwargs)
+        super(TrainDataLoader, self).__init__(datasets, *args, **kwargs)
         self.datasets = datasets
         self.num_batches = 0
 

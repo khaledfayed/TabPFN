d1e079475daab5f363635a6ad9c333847b6e5919
Khaled Fayed <khaledfayed1995@gmail.com>
2023-10-19 17:48:04 +0200

    try

--- tabpfn/lightning_train.py
+++ tabpfn/lightning_train.py
@@ -168,7 +168,7 @@ if __name__ == "__main__":
     datasets = load_OHE_dataset(auto_ml_dids_train, one_hot_encode=False)
     test_datasets = load_OHE_dataset(auto_ml_dids_val, one_hot_encode=False)
 
-    train_loader = TrainDataLoader(datasets, num_workers=1)
+    train_loader = DataLoader(zip(meta_dataset_loader3(datasets)), num_workers=1, batch_size=1)
     
     trainer = pl.Trainer(max_epochs=1000, log_every_n_steps=1, limit_train_batches=10, num_sanity_val_steps=0)
     trainer.fit(metaNet, train_loader, train_loader)

0ff43f906c837f1269792c74f86b948c9757826a
Khaled Fayed <khaledfayed1995@gmail.com>
2023-10-24 09:17:07 +0200

    relabel

--- tabpfn/run.sh
+++ tabpfn/run.sh
@@ -38,7 +38,7 @@ conda activate thesis
 # Running the job
 start=`date +%s`
 
-python train_new_try_meta.py --epochs 4000 --lr 0.0001 --weight_decay 0.0001 --name "1.random"
+python train_new_try_meta.py --epochs 4000 --lr 0.0001 --weight_decay 0.0001 --name "1.relabel"
 
 end=`date +%s`
 runtime=$((end-start))

e3c2247aeebe7f981c1c504793597ff05ed780e7
Khaled Fayed <khaledfayed1995@gmail.com>
2023-10-26 09:52:31 +0200

    0.1

--- tabpfn/run.sh
+++ tabpfn/run.sh
@@ -38,7 +38,7 @@ conda activate thesis
 # Running the job
 start=`date +%s`
 
-python train_new_try_meta.py --epochs 101 --lr 0.00002 --weight_decay 0.0001 --name "finetune"
+python train_new_try_meta.py --epochs 101 --lr 0.00001 --weight_decay 0.0001 --name "finetune"
 
 end=`date +%s`
 runtime=$((end-start))

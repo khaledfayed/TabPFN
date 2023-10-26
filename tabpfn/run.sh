d4ce5566005a7d4f3f4a4ec948f28b66f290e879
Khaled Fayed <khaledfayed1995@gmail.com>
2023-10-26 13:17:34 +0200

    fine tune relabel

--- tabpfn/run.sh
+++ tabpfn/run.sh
@@ -38,7 +38,7 @@ conda activate thesis
 # Running the job
 start=`date +%s`
 
-python train_new_try_meta.py --epochs 1001 --lr 0.00001 --weight_decay 0.0001 --name "fine tune drop"
+python train_new_try_meta.py --epochs 1001 --lr 0.00001 --weight_decay 0.0001 --name "fine tune relabel"
 
 end=`date +%s`
 runtime=$((end-start))

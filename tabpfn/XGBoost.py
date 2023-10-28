f4f5aa0d41677bebd1decfcd37d415aebdb5ddef
Khaled Fayed <khaledfayed1995@gmail.com>
2023-10-28 13:00:22 +0200

    maybe fix

--- tabpfn/XGBoost.py
+++ tabpfn/XGBoost.py
@@ -2,7 +2,6 @@ import xgboost as xgb
 from sklearn.model_selection import GridSearchCV
 import os
 import openml
-from sklearn.model_selection import train_test_split
 import numpy as np
 from meta_dataset_loader import load_OHE_dataset
 from evaluate_classifier import auto_ml_dids_test
@@ -19,14 +18,16 @@ for dataset in datasets:
             
     dataset_length = len(dataset['data'])
     
+    train_test_split = dataset_length * 0.7
+    
     dataset_indices = np.arange(dataset_length)
     rng.shuffle(dataset_indices)
     
     dataset['data'] = dataset['data'][dataset_indices]
     dataset['target'] = dataset['target'][dataset_indices]
     
-    fit_data = dataset['data'][:1000]
-    fit_target = dataset['target'][:1000]
+    fit_data = dataset['data'][:train_test_split]
+    fit_target = dataset['target'][:train_test_split]
     clf = xgb.XGBClassifier(tree_method='gpu_hist')
     # Define the parameter grid for cross-validation
     param_grid = {
@@ -41,5 +42,5 @@ for dataset in datasets:
     grid_search.fit(fit_data, fit_target)
     # Evaluate the best model on the test data
     best_model = grid_search.best_estimator_
-    test_score = best_model.score(dataset['data'][1000:], dataset['target'][1000:])
+    test_score = best_model.score(dataset['data'][train_test_split:], dataset['target'][train_test_split:])
     print("Test accuracy: {:.2f}".format(test_score))

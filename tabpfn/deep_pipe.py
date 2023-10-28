35a6ca5d7f2fae74cef43175c9d8c854c67cdb10
Khaled Fayed <khaledfayed1995@gmail.com>
2023-10-28 15:03:26 +0200

    try deep pipe

--- tabpfn/deep_pipe.py
+++ tabpfn/deep_pipe.py
@@ -1,23 +1,54 @@
-from deeppipe_api.deeppipe import load_data, openml, DeepPipe
+from deeppipe_api.deeppipe import  DeepPipe
 
-def deep_pipe_accuracy():
     
-    task = openml.tasks.get_task(task_id=146800)
-    X_train, X_test, y_train, y_test = load_data(task, fold=0)
+import os
+import numpy as np
+import openml
+from evaluate_classifier import auto_ml_dids_test
+from meta_dataset_loader import load_OHE_dataset
+
+
+openml.config.set_cache_directory(os.path.abspath('openml'))
+print(openml.config.get_cache_directory())
+
+# Load a specific dataset by its ID
+
+datasets = load_OHE_dataset([31],one_hot_encode=False)
+    
+rng = np.random.default_rng(seed=42)
+
+results = []
+
+for dataset in datasets:
+
+
+    # Split the data into training and test sets
+    dataset_length = len(dataset['data'])
+    
+    train_test_split = 512
+    
+    dataset_indices = np.arange(dataset_length)
+    rng.shuffle(dataset_indices)
+    
+    dataset['data'] = dataset['data'][dataset_indices]
+    dataset['target'] = dataset['target'][dataset_indices]
+    
+    fit_data = dataset['data'][:train_test_split]
+    fit_target = dataset['target'][:train_test_split]
+
     deep_pipe = DeepPipe(n_iters = 50,  #bo iterations
                         time_limit = 3600, #in seconds
                         apply_cv = True,
                         create_ensemble = False,
                         ensemble_size = 10,
                         )
-    deep_pipe.fit(X_train, y_train)
-    y_pred = deep_pipe.predict(X_test)
-    score = deep_pipe.score(X_test, y_test)
-    print("Test acc.:", score) 
+    deep_pipe.fit(fit_data, fit_target)
+    test_score = deep_pipe.score(dataset['data'][train_test_split:], dataset['target'][train_test_split:])
+    print("Test acc.:", test_score) 
 
-def main():
-
-    deep_pipe_accuracy()
-
-if __name__ == "__main__":
-    main()
+    # Print the best parameters and score
+    results.append(test_score)
+    
+formatted_data = '	'.join(map(str, results))
+print(formatted_data)
+    

75cd408f7f8e22e9621fde9b732c0889c2aee56b
Khaled Fayed <khaledfayed1995@gmail.com>
2023-10-28 13:02:43 +0200

    try higher split

--- tabpfn/XGBoost.py
+++ tabpfn/XGBoost.py
@@ -18,7 +18,7 @@ for dataset in datasets:
             
     dataset_length = len(dataset['data'])
     
-    train_test_split = int(dataset_length * 0.7)
+    train_test_split = int(dataset_length * 0.8)
     
     dataset_indices = np.arange(dataset_length)
     rng.shuffle(dataset_indices)

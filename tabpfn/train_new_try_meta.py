bbb6790a1031b82655027310264306e6b3499e4e
Khaled Fayed <khaledfayed1995@gmail.com>
2023-10-29 16:35:25 +0100

    try freeze

--- tabpfn/train_new_try_meta.py
+++ tabpfn/train_new_try_meta.py
@@ -94,13 +94,7 @@ def train(lr=0.0001, wandb_name='', num_augmented_datasets=0, epochs = 100, weig
         evaluate_classifier2(classifier, test_datasets, log= device != 'cpu')
     
     model.train()
-    
-#     for _, param in model.named_parameters():
-#         param.requires_grad = False
 
-# # Unfreeze the layers you want to train
-#     for name, param in list(model.named_parameters())[-32:]:
-#         param.requires_grad = True
         
     for e in range(epochs):
         
@@ -127,6 +121,13 @@ def train(lr=0.0001, wandb_name='', num_augmented_datasets=0, epochs = 100, weig
                 X_full = torch.cat(
                         [X_full,
                         torch.zeros((X_full.shape[0], X_full.shape[1], max_features - X_full.shape[2])).to(device)], -1)
+                
+                for _, param in model.named_parameters():
+                    param.requires_grad = False
+
+# Unfreeze the layers you want to train
+                for name, param in list(model.named_parameters())[-32:]:
+                    param.requires_grad = True
                 
                 criterion.weight=torch.ones(num_classes)
                 
